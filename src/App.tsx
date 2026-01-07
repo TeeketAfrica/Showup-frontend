0c8af71c4932ef05b7822971424f11877d6e6d6b
solinkz <solomoneyitene@gmail.com>
2026-01-07 14:28:20 +0100

    feat: implement payment confirmation page with ride details and a new success icon.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "@/components/app/home/HomePage";
 import { RoutePage } from "@/components/app/route-bus/RoutePage";
+import { PaymentPage } from "@/components/app/payment/PaymentPage";
 
 export function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="route" element={<RoutePage />} />
-      <Route path="payment" element={<HomePage />} />
+      <Route path="payment" element={<PaymentPage />} />
     </Routes>
   );
 }
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,16 +1,18 @@
 import Logo from "@/assets/Logo.svg";
 import { CardDescription } from "@/components/ui/card";
 
-export function Header() {
+export function Header({ noDescription }: { noDescription?: boolean }) {
   return (
     <div className="flex flex-col gap-3 items-center text-center">
       <h1>
         <img src={Logo} alt="Showup Logo" className="h-6" />
       </h1>
-      <CardDescription>
-        We help you find the best bus routes to reach your destination
-        effortlessly.
-      </CardDescription>
+      {!noDescription && (
+        <CardDescription>
+          We help you find the best bus routes to reach your destination
+          effortlessly.
+        </CardDescription>
+      )}
     </div>
   );
 }
