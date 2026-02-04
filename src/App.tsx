a0561bcdda659bf4b414758b2cb0c15a656c6ea2
Femi Akerele <lovinamercie@gmail.com>
2026-02-04 21:22:04 +0100

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-// import { ComponentExample } from "@/components/component-example";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HomePage } from "@/components/app/home/HomePage";
 import { RoutePage } from "@/components/app/route-bus/RoutePage";
 import { PaymentPage } from "@/components/app/payment/PaymentPage";
-import { useAppSelector } from "./hooks/reduxHooks";
-import { use, useEffect } from "react";
 
 export function App() {
-  const navigate = useNavigate();
-  const {auth: {exists}} = useAppSelector((s) => s);
 
-  console.log(exists)
-  
-  // useEffect(() => { 
-  //   if(exists){
-  //     navigate('/route')
-  //   }else{
-  //     navigate('/')
-  //   }
-  // }, [exists]);
   return (
     <Routes>
       <Route index element={<HomePage />} />
