0dd187a80528c36827ad1e0cd625582c5116b791
Femi Akerele <lovinamercie@gmail.com>
2026-02-15 01:27:07 +0100

    swited to env variable

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://showup-ninja.onrender.com/api/v1",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
